3ccd694beaf9564a354c1bb5c1083ee23ad5e9b9
mokdanbi <l26tempo@naver.com>
2022-10-13 14:15:13 +0900

    1013

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+const profile = {
+    name: "mok dan-bi",
+    email: "l26tempo@naver.com",
+    tel: "010-2640-1940"
+};
+
+export { profile };
+
+
+
+const portfolio = [
+    { id: 1, title: "hd01", type: "Responsive Web", font: ["Noto Sans Korea", "Playfair Display", "IM Fell English SC"], info: "브랜드의 원래 로고나 제품 패키지 디자인을 참고하여,\n 모던하고 딥한 이미지를 고취시킬 수 있는 다크모드로 디자인 했습니다.\n 무채색을 사용해 브랜드의 정체성을 드러냈고,\n 첫번째 섹션은 브랜드만이 가지고 있는 특이점이라고 생각해 타이포그라피로 디자인 했습니다.", color: ["#369", "#f00", "tomato"], skill: ["react", "react-router-dom", "axios", "redux-tollkit"], src: process.env.PUBLIC_URL + "/favicon.png", link: "http://mokdanbi.github.io/" },
+    { id: 2, title: "hd02", type: "Responsive Web", font: ["Noto Sans Korea", "Playfair Display", "IM Fell English SC"], info: "브랜드의 원래 로고나 제품 패키지 디자인을 참고하여,\n 모던하고 딥한 이미지를 고취시킬 수 있는 다크모드로 디자인 했습니다.\n 무채색을 사용해 브랜드의 정체성을 드러냈고,\n 첫번째 섹션은 브랜드만이 가지고 있는 특이점이라고 생각해 타이포그라피로 디자인 했습니다.", color: ["#369", "#f00", "tomato"], skill: ["react", "react-router-dom", "axios", "redux-tollkit"], src: process.env.PUBLIC_URL + "/favicon.png", link: "http://mokdanbi.github.io/" },
+    { id: 3, title: "hd03", type: "Responsive Web", font: ["Noto Sans Korea", "Playfair Display", "IM Fell English SC"], info: "브랜드의 원래 로고나 제품 패키지 디자인을 참고하여,\n 모던하고 딥한 이미지를 고취시킬 수 있는 다크모드로 디자인 했습니다.\n 무채색을 사용해 브랜드의 정체성을 드러냈고,\n 첫번째 섹션은 브랜드만이 가지고 있는 특이점이라고 생각해 타이포그라피로 디자인 했습니다.", color: ["#369", "#f00", "tomato"], skill: ["react", "react-router-dom", "axios", "redux-tollkit"], src: process.env.PUBLIC_URL + "/favicon.png", link: "http://mokdanbi.github.io/" }
+];
+
+export { portfolio };
